7408ba33d3fdb2b32a91e4515dc72de3015a2994
Juan David <jdgaravito317@gmail.com>
2021-03-24 16:53:50 -0500

    added projects

--- src/routes/portafolio/_projects.js
+++ src/routes/portafolio/_projects.js
@@ -1,34 +1,34 @@
 const projects = [
   {
     id: "01",
-    name: "Estudio Futurista",
-    slug: "estudio-futurista",
+    name: "Home Studio Colina",
+    slug: "home-studio-colina",
     category: "acond",
-    client: "Secreto",
+    client: "Privado",
     city: "Bogotá",
     country: "Colombia",
-    date: "2018-11-14 12:00",
+    date: "2020-08-14",
     tagline: "Iluminación y sonido",
     description:
-      "Diseño, Acondicionamiento y aislamiento acústico en home studio, uso de paneles entelados fonoabsorbentes con modulación personalizada e iluminación led, plafones acústicos entelados descolgados, difusores QRD con iluminación personalizada, paneles acústicos perforados con acabado en madera y trampas de bajos personalizadas en madera.",
+      "Planeación, diseño del proyecto de Home studio para produccion musical en Colina, se estableció el aislamiento acústico general al interior de un apartamento para evitar generar ruidos molestos a las habitaciones colindantes y así permitir el trabajo nocturno del productor e ingeniero de sonido, se utilizó mezcla entre mampostería y sistema liviano para brindar una mitigación de ruido ideal. Se realizó el Acondicionamiento acústico pensando principalmente en la parte visual, sin dejar de lado la funcionalidad acústica jugando con geometrías, iluminación y colores para generar sensación de calidez y comodidad controlando al mismo tiempo la escucha al interior de esta sala",
     mainImage: "/images/projects/proyecto4-1.jpeg",
     imgUrl1: "/images/projects/proyecto4-2.jpeg",
     imgUrl2: "/images/projects/proyecto4-3.jpeg",
     imgUrl3: "/images/projects/proyecto4-4.jpeg",
     imgUrl4: "/images/projects/proyecto4-5.jpeg",
-    attribute1Name: "Técnicas",
+    attribute1Name: "Tiempo de ejecución",
     attribute1:
-      "Diseño, Acondicionamiento y aislamientoa cústico",
-    attribute2Name: "Técnica de Acondicionamiento ",
+      "8 meses",
+    attribute2Name: "Técnicas utilizadas ",
     attribute2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
+      "Aislamiento acústico y acondicionamiento acústico",
     attribute3Name: "Elementos Instalados",
     attribute3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit.",
+      "Paneles entelados fonoabsorbentes con modulación personalizada e iluminación led, plafones acústicos entelados descolgados, difusores QRD con iluminación personalizada, paneles acústicos perforados con acabado en madera y trampas de bajos personalizadas en madera, mudo acústico doble en mampostería puerta acústica.",
   },
   {
     id: "02",
-    name: "Estudio Pablo Iglesia",
+    name: "Salas para postProducción de Tv Universidad Norte",
     slug: "estudio-pablo-iglesia",
     category: "aisla",
     client: "Nombre cliente",
@@ -37,7 +37,7 @@ const projects = [
     date: "2018-11-14 12:00",
     tagline: "Un espacio que vibra con estilo",
     description:
-      "Uismodolor, utpat erci a lam tis os, quip lummy, uipsuscidunt iusto nullandrem. Veliquam amconul veriliquat, is erciduisit molor tetummy na wisl laore sisi. Dionsenim to ate dolorem molor adiam. Enim ptatueros wiscip nissectet iurerosto veliquisl do eum, atum. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit, dolutatueros te amconul tio veleniat tie. Scillummy ver at. Feu eniatisit tionsequisim tio lummy it, dolortionum it. Iriustrud ero ros molent. Ullum diat nim modit. Eu lam hendrem esse duis.",
+      "Planeación, diseño del proyecto y ejecución  para dos salas de Postproducción de Tv en universidad Norte, Se realizó el Acondicionamiento acústico bajo elementos ya establecidos en la sala por la Universidad, Enfocándose principalmente en tener una iluminación tenue y colores neutros ya que en estas salas se realizan correciones de colores tenues, se utilizaron diferentes elementos maderosos para darle una sensación de calidez a la sala manteniendo un equilibrio audible en su interior.",
     mainImage: "/images/projects/proyecto3-1.jpeg",
     imgUrl1: "/images/projects/proyecto3-2.jpeg",
     imgUrl2: "/images/projects/proyecto3-3.jpeg",
@@ -45,18 +45,18 @@ const projects = [
     imgUrl4: "/images/projects/proyecto3-5.jpeg",
     attribute1Name: "Tiempo de Ejecución",
     attribute1:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
-    attribute2Name: "Técnica de Acondicionamiento ",
+      "5 meses",
+    attribute2Name: "Técnicas utilizadas ",
     attribute2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
+      "Acondicionamiento acústico",
     attribute3Name: "Elementos Instalados",
     attribute3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit.",
+      "Paneles entelados fonoabsorbentes con modulación personalizada con dilataciones en madera, fibra absorbente modulada en cielo raso con dilataciones en madera, difusores bidimensionales en madera, resonadores acústicos en madera y trampas de bajos personalizadas en madera.",
   },
   {
     id: "03",
-    name: "Home Studio",
-    slug: "Home-Studio",
+    name: "Home Studio Cedritos",
+    slug: "Home-Studio-Cedritos",
     category: "acond",
     client: "Secreto",
     city: "Bogotá",
@@ -64,7 +64,7 @@ const projects = [
     date: "2018-11-14 12:00",
     tagline: "Estudio en casa para musicos profesionales",
     description:
-      "Diseño, Acondicionamiento y aislamiento acústico en home studio, uso de difusores bidimensionales, paneles sono absorbentes entelados, resonadores acústicos y trampas de bajo personalizadas en madera.",
+      "Planeación, diseño y ejecución del proyecto de Home studio para producción musical, se estableció el aislamiento acústico general al interior de un apartamento para controlar tanto el ruido incidente como el ruido generado, para evitar cualquier tipo de intromisión tanto al interior como al exterior del recinto. Se realizó el Acondicionamiento acústico fusionando tanto la estética como la funcionalidad acústica, se utilizaron diferentes elementos como materiales para controlar todo tipo de problemas sonoros que se pudieran presentar brindando tanto calidez como calidad sonora a los espacios",
     mainImage: "/images/projects/proyecto3-1.jpeg",
     imgUrl1: "/images/projects/proyecto3-2.jpeg",
     imgUrl2: "/images/projects/proyecto2-5.jpeg",
@@ -72,26 +72,26 @@ const projects = [
     imgUrl4: "/images/projects/proyecto2-3.jpeg",
     attribute1Name: "Tiempo de Ejecución",
     attribute1:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
-    attribute2Name: "Técnica de Acondicionamiento ",
+      "6 meses",
+    attribute2Name: "Técnicas utilizadas ",
     attribute2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
+      "Aislamiento acústico y acondicionamiento acústico",
     attribute3Name: "Elementos Instalados",
     attribute3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit.",
+      "Difusores bidimensionales, paneles fono absorbentes entelados, resonadores acusticos y trampas de bajo personalizadas en madera, visores acústicos personalizados, muros acústicos, puertas acústicas con exclusa.",
   },
   {
     id: "04",
-    name: "Estudio rendercitos",
-    slug: "rendersitos-estudios",
+    name: "Salas para postProducción y producción musical publicitarios",
+    slug: "salas-para-postproduccion-musical",
     category: "acond",
-    client: "Estudios de audio profesional",
+    client: "Estudios de Postproducción",
     city: "Bogotá",
     country: "Colombia",
-    date: "2020-11-14 12:00",
-    tagline: "Un espacio que vibra con estilo",
+    date: "2020-11-14",
+    tagline: "Calidad al mejor precio",
     description:
-      "Uismodolor, utpat erci a lam tis os, quip lummy, uipsuscidunt iusto nullandrem. Veliquam amconul veriliquat, is erciduisit molor tetummy na wisl laore sisi. Dionsenim to ate dolorem molor adiam. Enim ptatueros wiscip nissectet iurerosto veliquisl do eum, atum. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit, dolutatueros te amconul tio veleniat tie. Scillummy ver at. Feu eniatisit tionsequisim tio lummy it, dolortionum it. Iriustrud ero ros molent. Ullum diat nim modit. Eu lam hendrem esse duis.",
+      "Planeación, diseño del proyecto y ejecución  para dos salas de Producción y Postproducción de de audio para publicidad, se estableció el aislamiento acústico general al interior de dos salas colindantes a espacios de trabajos para evitar generar interrupciones y/o filtraciones de ruidos molestas tanto a las personas que se encuentran trabajando y a las personas que se encuentran trabajando al interior de las salas, se utilizó mezcla entre mampostería y sistema liviano, ventanería acústica y puertas en vidrio acústico para brindar una mitigación de ruido ideal. Se realizó el Acondicionamiento acústico pensando principalmente en evitar el cansancio visual, utilizando elementos con colores iluminados que generan sensación de comodidad y calidez, controlando al interior la escucha de manera eficiente eficaz y fiel a las grabaciones realizadas.",
     mainImage: "/images/projects/proyecto1-1.jpeg",
     imgUrl1: "/images/projects/proyecto1-2.jpeg",
     imgUrl2: "/images/projects/proyecto1-3.jpeg",
@@ -99,13 +99,13 @@ const projects = [
     imgUrl4: "/images/projects/proyecto1-5.jpeg",
     attribute1Name: "Tiempo de Ejecución",
     attribute1:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
-    attribute2Name: "Técnica de Acondicionamiento ",
+      "7 meses",
+    attribute2Name: "Técnicas utilizadas ",
     attribute2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit.",
+      "Acondicionamiento acústico y aislamiento acústico",
     attribute3Name: "Elementos Instalados",
     attribute3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere nisi vel ornare aliquet. Eget blandit. Atum quis ptatueros eraessit utpat, esed vullutat, ud nisci. Conse ncidui augiametuer conullam quis ptatueros modit, minisl, ed essequating a digniamet. Esse ullam del aliquipsusci nismodit.",
+      "Enchape en piedra para difusión, difusores QRD en madera para difusión, paneles entelados sono absorbentes, paneles en madera con perforaciones personalizadas, resonadores en madera perforados, visor acústico personalizado y puerta acústica con doble esclusa en vidrio.",
   },
 ];
 
